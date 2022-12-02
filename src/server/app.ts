b20d9b9ef354b3aae5758a90d1e5611ae15e08a1
Shinichi Tomita <shinichi.tomita@gmail.com>
2022-12-02 09:58:19 +0900

    add error logging

--- src/server/app.ts
+++ src/server/app.ts
@@ -440,6 +440,7 @@ app.get(
       `);
     } catch (e) {
       const error_description = (e as any).response?.data?.error_description;
+      console.error(JSON.stringify((e as any).response, null, 2));
       console.error(
         [
           `error:${error_description}`,
